ae85a69f50c9b45244f8d281849d2cbc8dcd2c40
Mari <mari@Maris-MacBook-Air.local>
2021-11-22 14:16:34 +0200

    added Contact

---
+++ src/components/Contact.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="info">
+    <p>Telephone: 5555555</p><br>
+    <p>Mail: mail@mail.com</p><br>
+    <p>Location: City</p><br>
+    <p>Client service: 5555555</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contacts"
+}
+</script>
+
+<style scoped>
+
+.info {
+  text-align: center;
+  color: white;
+  margin-top: 50px;
+}
+
+template {
+  background-color: dodgerblue;
+}
+
+</style>
