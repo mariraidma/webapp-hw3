242cfe550101705166ff63ac49618446ac25175a
Mari <mari@Maris-MacBook-Air.local>
2021-11-22 14:13:09 +0200

    added Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="footer-container">
+    <div class="footer-text">
+      <a href="webdv-wa/contatcts.html">Contact us</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+.footer-container {
+  height: 100px;
+  width: 100%;
+  color: #1e90ff;
+}
+
+.footer-text a {
+  text-align: center;
+  font-size: 18px;
+  color: white;
+}
+
+
+
+</style>
